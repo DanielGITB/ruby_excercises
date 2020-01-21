dd3b35cc6c38fe2e75c6fb64e30946b11c7cbba0
Daniel B <1996.daniel.bryant@gmail.com>
2020-01-21 03:13:18 +0100

    Got my results but through incorrect code

--- my_group.rb
+++ my_group.rb
@@ -4,3 +4,9 @@ person_1 ={name: "Brad", gender: "male", age: 68}
 person_2 ={name: "Thad", gender: "male", age: 41}
 person_3 ={name: "Add", gender: "female", age: 43}
 
+ #tried using the shown method in the lesson but I did not manage to make it print any values from the keys between the vertical bars
+
+print "#{person_1[:name]} is a #{person_1[:age]} year old #{person_1[:gender]}\n"
+print "#{person_2[:name]} is a #{person_2[:age]} year old #{person_2[:gender]}\n"
+print "#{person_3[:name]} is a #{person_3[:age]} year old #{person_3[:gender]}"
+
