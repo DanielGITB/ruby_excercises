54f83cd82faa40f115615f90b7d86bce1f52cbc0
Daniel B <1996.daniel.bryant@gmail.com>
2020-01-21 01:55:11 +0100

    creating 3 person array

---
+++ my_group.rb
@@ -0,0 +1,6 @@
+my_group =["person_1", "person_2", "person_3"]
+
+person_1 ={name: "Brad", gender: "male", age: 68}
+person_2 ={name: "Thad", gender: "male", age: 41}
+person_3 ={name: "Add", gender: "female", age: 43}
+
