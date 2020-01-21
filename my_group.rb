efc10ea4bb3f8f6c7197107e344ddb87461c0b28
Daniel B <1996.daniel.bryant@gmail.com>
2020-01-21 22:20:19 +0100

    completed the taskt correctly

--- my_group.rb
+++ my_group.rb
@@ -1,12 +1,20 @@
-my_group =["person_1", "person_2", "person_3"]
+my_group =[]
 
-person_1 ={name: "Brad", gender: "male", age: 68}
-person_2 ={name: "Thad", gender: "male", age: 41}
-person_3 ={name: "Add", gender: "female", age: 43}
+person_1 ={name:"Brad", gender: "male", age: 68}
+person_2 ={name:"Thad", gender: "male", age: 41}
+person_3 ={name:"Add", gender: "female", age: 43}
 
- #tried using the shown method in the lesson but I did not manage to make it print any values from the keys between the vertical bars
+my_group.push(person_1)
+my_group.push(person_2)
+my_group.push(person_3)
 
-print "#{person_1[:name]} is a #{person_1[:age]} year old #{person_1[:gender]}\n"
-print "#{person_2[:name]} is a #{person_2[:age]} year old #{person_2[:gender]}\n"
-print "#{person_3[:name]} is a #{person_3[:age]} year old #{person_3[:gender]}"
+ 
+#print "#{person_1[:name]} is a #{person_1[:age]} year old #{person_1[:gender]}\n"
+#print "#{person_2[:name]} is a #{person_2[:age]} year old #{person_2[:gender]}\n"
+#print "#{person_3[:name]} is a #{person_3[:age]} year old #{person_3[:gender]}"
+
+my_group.each do |x|
+    puts "#{x[:name]} is a #{x[:age]} year old #{x[:gender]}."
+end
+
 
